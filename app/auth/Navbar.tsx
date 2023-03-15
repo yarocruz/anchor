266a4ae2c59ea926ce6e33fa77bfae503847e60a
yarocruz <yarocruz@gmail.com>
2023-03-15 12:47:27 -0400

    add orange navbar colors and second quick tour link

--- app/auth/Navbar.tsx
+++ app/auth/Navbar.tsx
@@ -5,36 +5,40 @@ import SignIn from "@/app/auth/SignIn";
 import Logout from "@/app/auth/Logout";
 
 export default async function Navbar() {
-    const session = await getServerSession(authOptions)
-    console.log("from the navbar", session)
+    const session = await getServerSession(authOptions);
     return (
-        <div className="navbar bg-base-300">
-            <div className="flex-1">
-                <a className="btn btn-link normal-case text-xl text-orange-600">hypertext</a>
-            </div>
-            {session ? (
-                <div className="flex-none gap-2">
-                    <div className="dropdown dropdown-end">
-                        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                            <div className="w-10 rounded-full">
-                                <img src={session.user?.image || ""} alt="User profile image"/>
-                            </div>
-                        </label>
-                        <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
-                            <li>
-                                <a className="justify-between">
-                                    Profile
-                                    <span className="badge">New</span>
-                                </a>
-                            </li>
-                            <li><a>Settings</a></li>
-                            <Logout />
-                        </ul>
-                    </div>
+        <div className="navbar bg-orange-500 text-white">
+            <div className="container mx-auto">
+                <div className="flex-1">
+                    <a href="#" className="normal-case text-xl">Hypertext</a>
                 </div>
-            ) : (
-                <SignIn />
+                <div className="flex-2">
+                    <a href="#" className="px-3">Quick Tour</a>
+                </div>
+                {session ? (
+                    <div className="flex-none gap-2">
+                        <div className="dropdown dropdown-end">
+                            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                                <div className="w-10 rounded-full">
+                                    <img src={session.user?.image || ""} alt="User profile image"/>
+                                </div>
+                            </label>
+                            <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+                                <li>
+                                    <a className="justify-between">
+                                        Profile
+                                        <span className="badge">New</span>
+                                    </a>
+                                </li>
+                                <li><a>Settings</a></li>
+                                <Logout />
+                            </ul>
+                        </div>
+                    </div>
+                ) : (
+                    <SignIn />
                 )}
+            </div>
         </div>
     )
 }
