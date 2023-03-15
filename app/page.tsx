8c0e01cc333fd07fc1b1b278ff53f98a477bef9b
yarocruz <yarocruz@gmail.com>
2023-03-15 15:48:37 -0400

    brough in getServerSession to show LinkSubmitForm only if we're logged in

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import LinkSubmitForm from "./components/LinkSubmitForm";
-
+import { getServerSession} from "next-auth";
+import { authOptions } from "../pages/api/auth/[...nextauth]";
 
 async function getLinks() {
   const response = await fetch(`${process.env.BASE_URL}/api/getLinks`, {cache: "no-store"})
@@ -9,21 +10,27 @@ async function getLinks() {
   return response.json();
 }
 export default async function Home() {
+    const session = await getServerSession(authOptions);
 
     const data: { id: string, url: string }[] = await getLinks()
   return (
-        <main className="container mx-auto my-2 w-auto">
+        <main className="container mx-auto my-2 w-auto p-2">
 
-            <LinkSubmitForm />
+            {session ? ( <LinkSubmitForm /> ) : null}
 
-            <ul className="space-y-2">
+            <div className="tabs mt-10 border-b-4 border-orange-600">
+                <a className="tab bg-orange-400 text-white mr-1.5">Recent Links</a>
+                <a className="tab bg-orange-400 text-white">Tags</a>
+            </div>
+
+            <ul className="my-5">
                 {data.map((link) => (
                     <li
-                        className="break-words"
+                        className="break-words py-3 border-b-gray-100 border-b-2"
                         key={link.id}
                     >
                         <a
-                            className="text-blue-500"
+                            className="text-orange-600"
                             href={link.url}
                         >{link.url}</a></li>
                 ))}
