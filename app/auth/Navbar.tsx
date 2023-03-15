2147ca4121a38f9417fea8a74abf37d2aef68c15
yarocruz <yarocruz@gmail.com>
2023-03-15 15:46:45 -0400

    fixed avatar menu text and removed some options

--- app/auth/Navbar.tsx
+++ app/auth/Navbar.tsx
@@ -7,8 +7,8 @@ import Logout from "@/app/auth/Logout";
 export default async function Navbar() {
     const session = await getServerSession(authOptions);
     return (
-        <div className="navbar bg-orange-500 text-white">
-            <div className="container mx-auto">
+        <div className="navbar bg-orange-600 text-white">
+            <div className="container mx-auto p-2">
                 <div className="flex-1">
                     <a href="#" className="normal-case text-xl">Hypertext</a>
                 </div>
@@ -23,14 +23,12 @@ export default async function Navbar() {
                                     <img src={session.user?.image || ""} alt="User profile image"/>
                                 </div>
                             </label>
-                            <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+                            <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52 text-black">
                                 <li>
                                     <a className="justify-between">
                                         Profile
-                                        <span className="badge">New</span>
                                     </a>
                                 </li>
-                                <li><a>Settings</a></li>
                                 <Logout />
                             </ul>
                         </div>
