eb8e50875dec8666c495e4fa0f3e18cbf8aeadd5
yarocruz <yarocruz@gmail.com>
2023-03-15 12:50:47 -0400

    removed defatult css, added container classes, and fix responsive issue with list items

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-slate-50">
+      <body>
         <Navbar />
       {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import LinkSubmitForm from "./components/LinkSubmitForm";
 
+
 async function getLinks() {
   const response = await fetch(`${process.env.BASE_URL}/api/getLinks`, {cache: "no-store"})
   if (!response.ok) {
@@ -11,14 +12,14 @@ export default async function Home() {
 
     const data: { id: string, url: string }[] = await getLinks()
   return (
-        <main className="container mx-auto">
-            <h1>Home</h1>
+        <main className="container mx-auto my-2 w-auto">
 
             <LinkSubmitForm />
 
             <ul className="space-y-2">
                 {data.map((link) => (
                     <li
+                        className="break-words"
                         key={link.id}
                     >
                         <a
