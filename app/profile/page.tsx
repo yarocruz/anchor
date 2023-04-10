68d6811ffdfb1276dd69c91815efce8300f03e0b
yarocruz <yarocruz@gmail.com>
2023-04-10 11:32:23 -0400

    add a loading state on profile page. no longer showing that user doesnt have any links

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -20,16 +20,16 @@ export default function Profile() {
     // show links that belong to current logged-in user
     const [data, setData] = useState<LinkData[] | []>([])
     const [session, setSession] = useState<Session | null>(null)
+    const [loading, setLoading] = useState<boolean>(true)
     const router = useRouter()
 
     useEffect(() => {
         const fetchSession = async () => {
             const sessionData = await getSession()
-            console.log(sessionData)
             setSession(sessionData)
             const links = await getUserLinks(sessionData?.user?.email!)
-            console.log(links)
             setData(links)
+            setLoading(false)
         }
         if (data.length === 0) {
             fetchSession()
@@ -55,27 +55,28 @@ export default function Profile() {
         <main className="container mx-auto my-2 w-auto p-2">
             <h1>Your Saved Links</h1>
             <div className="my-5">
-                {data.length ? data.map((link) => (
-                    <div key={link.id} className="py-3 border-b-gray-100 border-b-2">
-                        <p
-                            className="break-words"
-                        >
-                            <a
-                                className="text-orange-600"
-                                target="_blank"
-                                href={link.url}
-                            >{link.title ? link.title : link.url}</a>
-                        </p>
-                        <p>{link.description ? link.description : null}</p>
-                        <p>{link.tags ? link.tags.map(tag => (
-                            <Link href={`/tags/${tag.name}`} key={tag.id} id={tag.id} className="mr-3 text-amber-800 hover:text-amber-500">{tag.name}</Link>
-                        )) : null}</p>
-                        <p className="my-1">
-                            <span onClick={() => onEdit(link.id)} className="hover:text-amber-300 text-white btn-sm bg-amber-500 rounded py-1 mr-3 cursor-pointer">edit</span>
-                            <span onClick={() => onDelete(link.id)} className="hover:text-red-500 text-white btn-sm bg-amber-500 rounded py-1 cursor-pointer">delete</span>
-                        </p>
-                    </div>
-                )) : <p className="max-w-md">
+                {loading ? ( <p>Loading...</p> ) :
+                    data.length ? data.map((link) => (
+                        <div key={link.id} className="py-3 border-b-gray-100 border-b-2">
+                            <p
+                                className="break-words"
+                            >
+                                <a
+                                    className="text-orange-600"
+                                    target="_blank"
+                                    href={link.url}
+                                >{link.title ? link.title : link.url}</a>
+                            </p>
+                            <p>{link.description ? link.description : null}</p>
+                            <p>{link.tags ? link.tags.map(tag => (
+                                <Link href={`/tags/${tag.name}`} key={tag.id} id={tag.id} className="mr-3 text-amber-800 hover:text-amber-500">{tag.name}</Link>
+                            )) : null}</p>
+                            <p className="my-1">
+                                <span onClick={() => onEdit(link.id)} className="hover:text-amber-300 text-white btn-sm bg-amber-500 rounded py-1 mr-3 cursor-pointer">edit</span>
+                                <span onClick={() => onDelete(link.id)} className="hover:text-red-500 text-white btn-sm bg-amber-500 rounded py-1 cursor-pointer">delete</span>
+                            </p>
+                        </div>
+                    )) : <p className="max-w-md">
                         Hi there {session?.user?.name}! You haven't added any Links.
                         Be sure to go back to the <Link className="text-orange-600" href="/">main page </Link>
                         and start adding some links.</p>}
