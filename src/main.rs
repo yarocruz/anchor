68e99360c33bf4096373d93ae6ceb852b72cf1d0
yarocruz <yarocruz@gmail.com>
2024-01-28 12:41:19 -0500

    add the barebones server and serve static assets

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
 use axum::{
     response::Html,
-    routing::get,
+    routing::{get, get_service},
     Router
 };
+
+use tower_http::services::{ServeDir, ServeFile};
+
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/", get(handler));
+        .route("/hello", get(handler))
+        .fallback_service(
+            Router::new().nest_service("/", get_service(ServeDir::new("./assets")))
+        );
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("Starting Server");
     axum::serve(listener, app).await.unwrap();
 }
 
 async fn handler() -> Html<&'static str> {
     Html("<h1>Hello World!</h1>")
 }
+
+
